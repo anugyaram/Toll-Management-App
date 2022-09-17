5fa95c54fb0462a9ca0d5fa0fad7aa7527cc706c
Anugya Ram <anugyaram08@gmail.com>
2022-09-17 11:15:48 +0530

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <div className="heading">
+            <p>Toll Management Application</p>
+            <hr/>
+        </div>
+    )
+}
+export default Header;
