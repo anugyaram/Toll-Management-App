ab141e96972fcd1639c2a8a15f0d55cfb30fa753
Anugya Ram <anugyaram08@gmail.com>
2022-09-17 23:53:57 +0530

    Page Transfer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Header from './components/Header';
-import List from './components/List';
-import Select from './components/Select';
-import Input from './components/Input';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <List/>
-      <Select/>
-      <Input placeholder={"Single Journey"} type={'number'} />
+      <Main />
     </div>
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import List from '../components/List';
+
+function Main(){
+    return (
+        <div className="Main">
+          <Header/>
+          <List/>
+        </div>
+      );
+}
+export default Main;
