19e0a0888f03a08690fa4556ee3d72aaeef78f97
Anugya Ram <anugyaram08@gmail.com>
2022-09-18 18:00:34 +0530

    Header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,12 @@
 import './Header.css';
-import Search from './SearchBar';
-import Button from './Button';
-import Dropdown from './Dropdown';
-import AddToll from '../pages/AddToll';
-import NewEntry from '../pages/NewEntry';
 
 function Header(){
     return(
         <div className="heading">
             <p>Toll Management Application</p>
             <hr/>
-            <div className='nav'>
-                <div className='objects'>
-                    <div>
-                        <p>Toll entries/Vehicle entries</p>
-                    </div>
-                    <div>
-                        <Dropdown/>
-                    </div>
-                    <div>
-                        <Search/>
-                    </div>
-                </div>
-                <div className='buttons'>
-                    <div className='button' >
-                        <Button onClick={vehicleEntryEle}>Add vehicle entry</Button>
-                    </div>
-                    <div className='button' >
-                        <Button onClick={showTollEle}>Add new toll</Button>
-                    </div>
-                    <div className='button'>
-                        <Button>View all tolls</Button>
-                    </div>
-                </div>
-            </div>
-            <div className='show-toll'>
-                <AddToll />
-            </div>
-            <div className='vehicle-entry'>
-                <NewEntry />
-            </div>
         </div>
     )
-}
-
-let modal1 = document.querySelector('.show-toll');
-let modal2 = document.querySelector('.vehicle-entry');
-
-let showTollEle = () => document.querySelector('.show-toll').style.display = "block";
-let vehicleEntryEle = () => document.querySelector('.vehicle-entry').style.display = "block";
-
-window.onclick = function(event) {
-    if (event.target === modal1) {
-        modal1.style.display = "none";
-    }
-    if(event.target === modal2){
-        modal2.style.display = "none";
-    }
 }
 
 export default Header;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,66 @@
 import Header from '../components/Header';
+import Search from '../components/SearchBar';
+import Button from '../components/Button';
+import Dropdown from '../components/Dropdown';
+import AddToll from './AddToll';
+import NewEntry from './NewEntry';
 import List from '../components/List';
 
+import './Main.css';
 
 function Main(){
     return (
         <div className="Main">
-          <Header/>
+          <Header />
+          <div className='nav'>
+                <div className='objects'>
+                    <div>
+                        <p>Toll entries/Vehicle entries</p>
+                    </div>
+                    <div>
+                        <Dropdown/>
+                    </div>
+                    <div>
+                        <Search/>
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <div className='button' >
+                        <Button onClick={vehicleEntryEle}>Add vehicle entry</Button>
+                    </div>
+                    <div className='button' >
+                        <Button onClick={showTollEle}>Add new toll</Button>
+                    </div>
+                    <div className='button'>
+                        <Button>View all tolls</Button>
+                    </div>
+                </div>
+            </div>
+            <div className='show-toll'>
+                <AddToll />
+            </div>
+            <div className='vehicle-entry'>
+                <NewEntry />
+            </div>
           <List/>
         </div>
       );
 }
+
+let modal1 = document.querySelector('.show-toll');
+let modal2 = document.querySelector('.vehicle-entry');
+
+let showTollEle = () => document.querySelector('.show-toll').style.display = "block";
+let vehicleEntryEle = () => document.querySelector('.vehicle-entry').style.display = "block";
+
+window.onclick = function(event) {
+    if (event.target === modal1) {
+        modal1.style.display = "none";
+    }
+    if(event.target === modal2){
+        modal2.style.display = "none";
+    }
+}
+
 export default Main;
+
